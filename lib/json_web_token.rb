34b75907f00345a1e2eec12b825cfbd78c0eacc2
John Diego <johnoliveirasilva9@gmail.com>
2021-09-22 23:24:06 -0300

    Setup tokens controller

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -4,11 +4,11 @@ class JsonWebToken
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET_KEY, nil)
+    JWT.encode(payload, SECRET_KEY)
   end
 
   def self.decode(token)
-    decode = JWT.decode(token, SECRET_KEY, nil, false).first
+    decode = JWT.decode(token, SECRET_KEY).first
     HashWithIndifferentAccess.new decode
   end
 end
