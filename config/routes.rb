c9504b065295e11d22f4ea0c16dd67f98dc13481
John Diego <johnoliveirasilva9@gmail.com>
2021-10-05 21:31:54 -0300

    Finishes modeling the product model along with user associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :tokens, only: [:create]
-    end
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Api definition
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: [:show, :create, :update, :destroy]
+      resources :tokens, only: [:create]
+      resources :products, only: [:show, :index]
     end
   end
 end
