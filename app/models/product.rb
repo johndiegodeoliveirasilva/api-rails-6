35078415b41aebdb4af77af601709160ac41d434
John Diego <johnoliveirasilva9@gmail.com>
2021-10-21 00:16:50 -0300

    Assiciates products and orders with a placements model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   validates :title, :user_id, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
   belongs_to :user
+  has_many :placements, dependent: :destroy
+  has_many :orders, through: :placements
 
   # Scope
   scope :filter_by_title, lambda {|keyword|
