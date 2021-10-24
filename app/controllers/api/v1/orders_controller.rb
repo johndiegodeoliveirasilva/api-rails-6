cdd16929d4c40a9b0a4c14a84ae18595ead7dfcf
John Diego <johnoliveirasilva9@gmail.com>
2021-10-24 14:28:50 -0300

    Allows the order to be placed along with product quantity

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -16,18 +16,18 @@ class Api::V1::OrdersController < ApplicationController
 
   def create
     order = current_user.orders.build(order_params)
-
+    order.build_placements_with_product_ids_and_quantities(order_params:[:product_ids_and_quantities])
     if order.save
       OrderMailer.send_confirmation(order).deliver
       render json: order, status: 201
     else
-      render json: { errors: order.errors }, status: 422
+      render json: { errors: order.errors }, status: :unprocessable_entity
     end
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:total, product_ids: [])
+    params.require(:order).permit(product_ids_and_quantities: [:product_id, :quantity])
   end
 end
