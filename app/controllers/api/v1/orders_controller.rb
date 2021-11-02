0a768df38f4a73c2a5d9cbe89d8d760ac8b68410
John Diego <johnoliveirasilva9@gmail.com>
2021-11-02 11:36:19 -0300

    adds Quantity to Placements

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -15,8 +15,9 @@ class Api::V1::OrdersController < ApplicationController
   end
 
   def create
-    order = current_user.orders.build(order_params)
-    order.build_placements_with_product_ids_and_quantities(order_params:[:product_ids_and_quantities])
+    order = Order.create! user: current_user
+    order.build_placements_with_product_ids_and_quantities(
+      order_params:[:product_ids_and_quantities])
     if order.save
       OrderMailer.send_confirmation(order).deliver
       render json: order, status: 201
